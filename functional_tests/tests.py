369038eae5384e14a95cf6b706736d5ea99650f5
Keif <mashidin@hotmail.com>
2014-08-03 13:55:11 -0400

    Made functional_tests an app, use LiveServerTestCase instead of just unittest.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # I order to view a to-do app, I go to the to-list app's homepage
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # I notice the page title and header mention to-do-lists
         self.assertIn("To-Do", self.browser.title)
@@ -61,5 +61,3 @@ class NewVisitorTest(unittest.TestCase):
         # I visit the URL and my to-list is still there
 
 
-if __name__ == "__main__":
-    unittest.main(warnings="ignore")
