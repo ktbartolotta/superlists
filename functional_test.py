256bc4230f260fa231b09461f8f8992c3939c67a
Keif <mashidin@hotmail.com>
2014-08-03 09:45:20 -0400

    Added functional test to capture second added item.

--- functional_test.py
+++ functional_test.py
@@ -36,23 +36,27 @@ class NewVisitorTest(unittest.TestCase):
         # Buy peacock
         # feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-
+        
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(
-                any(row.text == "1: Buy peacock feathers" for row in rows),
-                "New to-do item did not appear in table"
-        )
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
 
         # There is still a textbox inviting me to add a another item.
         # I enter "Use peacock feathers to make a fly"
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on my list
-
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn("2: Use peacock feathers to make a fly", 
+            [row.text for row in rows])
+        
         # I see that the site has generated a unique URL for my list and an 
         # explanation that this is to get back to this list
-
+        self.fail("Finish the test!")
         # I visit the URL and my to-list is still there
 
 
