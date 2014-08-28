281172c9fa4b6c3523a8391e81146bf57f0bffd6
Keif <mashidin@hotmail.com>
2014-08-27 20:28:29 -0400

    fixed functional tests to connect to live server url.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,7 +10,7 @@ class NewVisitorTest(LiveServerTestCase):
     def setUpClass(cls):
         for arg in sys.argv:
             if "liveserver" in arg:
-                cls.serverurl = "http://" + arg.split("=")[1]
+                cls.server_url = "http://" + arg.split("=")[1]
                 return
         super().setUpClass()
         cls.server_url = cls.live_server_url
